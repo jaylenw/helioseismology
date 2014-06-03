b3eaa722637ca77bfc7f0054ce78b46c542ef0f4
jaylenw <mr2cleanj@live.com>
2014-06-02 22:42:07 -0700

    discovere 'm', added explanation

--- classifications_of_modes.py
+++ classifications_of_modes.py
@@ -1,13 +1,22 @@
+""" If using my mental model from quantum correctly
+    and applying it to finding these modes, 'n' is the energy mode (energy
+    level). 'l' is the angular degree.  The rotation splitting modes and angular
+    degrees all depend on 'n'.
+
+"""
+
+
 n=4
 N = ((-1*n) -1)
-l_list = []
+m_list = []
 
+#below we are building the rotation splitting, 'm' , known as m
 for number in xrange(n, N, -1):
 
-    l_list.append(number)
+    m_list.append(number)
     print number
 
 
-print l_list
+print m_list
 
 print N
