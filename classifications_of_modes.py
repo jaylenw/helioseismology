c88c5b561fefc3b5f3f1d27c1b8ce5efe98573e9
jaylenw <mr2cleanj@live.com>
2014-06-02 23:12:13 -0700

    cleaned up print statements

--- classifications_of_modes.py
+++ classifications_of_modes.py
@@ -1,4 +1,5 @@
-""" If using my mental model from quantum correctly
+"""
+    If using my mental model from quantum correctly
     and applying it to finding p and g modes of radial oscillations, 'n' is the
     energy mode (energy level). 'l' is the angular degree.  The rotation
     splitting modes and angular degrees all depend on 'n'.
@@ -6,22 +7,26 @@
 """
 
 
-n=4
-m_list = []
-l_list = []
+n=5
 
-#below we are building the rotation splitting modes, 'm' , known as m
-for number in xrange((-1*n), (n+1)):
+print "Energy mode is",n,"."
 
-    m_list.append(number)
-    print number
+#initializing lists for angular and splitting modes
+m_list = []      #splitting modes
+l_list = []      #angular modes
 
-
-print m_list
 
 #below we are building the angular energy mode
 for number in xrange(0, (n+1)):
 
     l_list.append(number)
 
-print l_list
+print "Angular degrees are...", "\n", l_list
+
+
+#below we are building the rotation splitting modes, 'm' , known as m
+for number in xrange((-1*n), (n+1)):
+
+    m_list.append(number)
+
+print "Rotation splitting modes are...", "\n", m_list
