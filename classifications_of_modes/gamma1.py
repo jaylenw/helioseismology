927d3b08d58c23dc1d6550a1cb62195944526f86
jaylenw <mr2cleanj@live.com>
2014-06-06 00:00:07 -0700

    adjusted comment

--- classifications_of_modes/gamma1.py
+++ classifications_of_modes/gamma1.py
@@ -2,5 +2,13 @@
 
   I will be assuming the Sun is is made up of all hydrogen just to see how this
   works.  I will be using pressure and density that I looked up for the Sun.
-  Although this may change.
+  Although this may change.  Let's assume the Sun is made up of a monatomic
+  ideal gas, therefore gamma = 5/3.
+
+  (P*V)^gamma = constant, P is pressure, V is volume.
+
+  gamma = ( C_sub(p) / C_sub(v) ) = ( f +2 / f ),
+
+  where f is the number of degrees of freedom of the gas. For monoatomic case,
+  the number of degrees of freedom are 3. so (2+3)/3 = 5/3
 """
