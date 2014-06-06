5c35b22ff63b36ddda46d95cf6b1ddcdbbe3c58f
jaylenw <mr2cleanj@live.com>
2014-06-05 22:49:33 -0700

    trying to see where im going

--- classifications_of_modes/gamma1.py
+++ classifications_of_modes/gamma1.py
@@ -1,1 +1,6 @@
-"""Gamma one"""
+"""Gamma one
+
+  I will be assuming the Sun is is made up of all hydrogen just to see how this
+  works.  I will be using pressure and density that I looked up for the Sun.
+  Although this may change.
+"""
