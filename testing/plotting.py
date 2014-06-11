b9aeacf681c02959839f7d276c5b81cc0a88b1a9
jaylenw <mr2cleanj@live.com>
2014-06-11 16:48:00 -0700

    initial commit of file to plot

---
+++ testing/plotting.py
@@ -0,0 +1,4 @@
+# Matplotlib can not plot in interactive mode.  Only when running a .py file.
+
+""" I am going to see if I can pull in data from main.py to here and then
+display the graph/plot/spectrum """
