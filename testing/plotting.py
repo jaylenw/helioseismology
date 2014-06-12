1c46f22e2f93ab0e3d8cb8cb65240e0563a803ec
jaylenw <mr2cleanj@live.com>
2014-06-11 19:30:53 -0700

    everything working as planned for plotting, good save

--- testing/plotting.py
+++ testing/plotting.py
@@ -8,8 +8,13 @@ display the graph/plot/spectrum """
 
 def plotomegaP(l_list, omegaP_list):
 
-  plt.plot(l_list, omegaP_list)
-  #plt.ylablel('Omega P Values')
+  plt.title("Angular Degrees vs. Omega P Modes")
+  plt.grid(True) #just adding a dotted grid for fun
+  plt.plot(l_list, omegaP_list, 'bo') #l_list is x-axis, omegaP_list is y-axis)
+  #the 'bo' just means don't use default line plot, but use blue filled circles
+
+  plt.xlabel('Angular Degrees') #labeled the axis
+  plt.ylabel('Omegas')          #labeled the axis
   plt.show()
 
   return
@@ -17,8 +22,13 @@ def plotomegaP(l_list, omegaP_list):
 
 def plotomegaG(l_list, omegaG_list):
 
-  plt.plot(l_list, omegaG_list)
-  #plt.ylabel("Omega G Values")
+  plt.title("Angular Degrees vs. Omega G Modes")
+  plt.grid(True) #just adding a dotted grid for fun
+  plt.plot(l_list, omegaG_list, 'bo')
+  #the 'bo' just means don't use default line plot, but use blue filledc circles
+
+  plt.xlabel('Angular Degrees') #labeled the axis
+  plt.ylabel('Omegas')          #labeled the axis
   plt.show()
 
   return
