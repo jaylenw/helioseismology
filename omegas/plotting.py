142fab1c8250dece42eac0bd6fe121a3e29d545a
jaylenw <mr2cleanj@live.com>
2014-06-14 16:42:51 -0700

    fixed imaginary plot issue, good save

--- omegas/plotting.py
+++ omegas/plotting.py
@@ -1,4 +1,7 @@
 import matplotlib.pyplot as plt  #importing necessary modules to plot
+import numpy as np               #importing numpy to help convert list to array
+                                 #to obtain imaginary portion of the list
+                                 #look at plotomegaG fxn
 
 
 # Matplotlib can not plot in interactive mode.  Only when running a .py file.
@@ -34,13 +37,22 @@ def plotomegaGsqrd(l_list, omegaGsqrd_list):
 
 def plotomegaG(l_list, omegaG_list):
 
+  """Below taking necessary process to convert the list to array.  Numpy allows
+  you to pull imaginary components from arrays. I'm converting the list that has
+  imaginary list to an array.  Then acting on the array to pull the numbers from
+  the imaginary components so that matplotlib can successfully graph it"""
+
+  omegaG_array = np.array(omegaG_list)  #converting list to new array
+  omegaG_array = omegaG_array.imag      #returning imaginary components without
+                                        #the imaginary type. ie. convert to float
+
   plt.title("Angular Degrees vs. Omega G Modes")
   plt.grid(True) #just adding a dotted grid for fun
-  plt.plot(l_list, omegaG_list, 'bo')
+  plt.plot(l_list, omegaG_array, 'bo')
   #the 'bo' just means don't use default line plot, but use blue filled circles
 
   plt.xlabel('Angular Degrees') #labeled the axis
-  plt.ylabel('Omegas')          #labeled the axis
+  plt.ylabel('Omegas, in Imaginary Results')          #labeled the axis
   plt.show()
 
   return
